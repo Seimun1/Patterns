b696ec67470161e8754848651f92d0777bd0312a
Galina <gana88tit@gmail.com>
2024-01-24 16:06:48 +0300

    change project

--- src/test/java/ru/netology/delivery/DeliveryTest.java
+++ src/test/java/ru/netology/delivery/DeliveryTest.java
@@ -34,7 +34,7 @@ class DeliveryTest {
         $("[data-test-id='name'] input").setValue(validUser.getName());
         $("[data-test-id='phone'] input").setValue(validUser.getPhone());
         $("[data-test-id='agreement']").click();
-        $(byText("Забронировать")).click();
+        $(byText("Запланировать")).click();
         $(byText("Успешно!")).shouldBe(visible, Duration.ofSeconds(15));
         $("[data-test-id='success-notification'] .notification__content")
                 .shouldHave(exactText("Встреча успешно запланирована на  " + firstMeetingDate))
